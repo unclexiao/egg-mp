9e0755861c599ad9a0eef2af82e988aecc9cfba1
unclexiao <449129722@qq.com>
2019-04-16 21:25:33 +0800

    fix bug: `appId` of 'undefined'

--- app/service/wechat.js
+++ app/service/wechat.js
@@ -12,8 +12,8 @@ class WechatService extends Service {
      * @description 登录凭证校验
      * @link https://developers.weixin.qq.com/miniprogram/dev/api/code2Session.html?search-key=jscode2session
      */
-    const { appId, appSecret } = this.app.config.wechat;
-    const url = `${jscode2sessionUri}?appid=${appId}&secret=${appSecret}&js_code=${code}&grant_type=authorization_code`;
+    const { appId, appSecret } = this.app.config.mp;
+    const url = `${jscode2sessionUri}?appid=${appId}&mp=${appSecret}&js_code=${code}&grant_type=authorization_code`;
     const res = await this.ctx.curl(url, { dataType: 'json' });
     return res.data;
   }
@@ -23,17 +23,18 @@ class WechatService extends Service {
      * @description 获取小程序全局唯一后台接口调用凭据
      * @link https://developers.weixin.qq.com/miniprogram/dev/api/getAccessToken.html
      */
-    const { appId, appSecret } = this.app.config.wechat;
+    const { appId, appSecret } = this.app.config.mp;
     const url = `${tokenUri}?grant_type=client_credential&appid=${appId}&secret=${appSecret}`;
     const res = await this.ctx.curl(url, { dataType: 'json' });
     return res.data;
   }
 
-  async decryptData(appId, sessionKey, encryptedData, iv) {
+  async decryptData(sessionKey, encryptedData, iv) {
     /**
      * @description 加密数据解密算法
      * @link https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/signature.html
      */
+    const { appId } = this.app.config.mp;
     const sessionKeyBuffer = new Buffer.from(sessionKey, "base64");
     const encryptedDataBuffer = new Buffer.from(encryptedData, "base64");
     iv = new Buffer.from(iv, "base64");
@@ -47,7 +48,7 @@ class WechatService extends Service {
       throw new Error("Illegal Buffer");
     }
     if (decoded.watermark.appid !== appId) {
-      throw new Error("Illegal Buffer");
+      throw new Error("Illegal Appid");
     }
     return decoded;
   }
